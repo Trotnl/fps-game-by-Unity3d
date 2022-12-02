4eba417cbd0a405cb0d050fdc1814ed4378dd45f
Trotnl <115418181+Trotnl@users.noreply.github.com>
2022-12-02 19:32:10 +0800

    Add files via upload

---
+++ Assets/Scripts/Sway.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MiYu
+{
+
+    public class Sway : MonoBehaviour
+    {
+        #region Variables
+
+        public float intensity;
+        public float smooth;
+
+        private Quaternion origin_rotation;
+
+        #endregion
+
+        #region Callbacks
+
+        void Start()
+        {
+            origin_rotation = transform.localRotation;
+        }
+
+        private void Update()
+        {
+            UpdateSway();
+        }
+        #endregion
+
+        #region Private Methods
+
+        private void UpdateSway()
+        {
+            //controls
+            float t_x_mouse = Input.GetAxis("Mouse X");
+            float t_y_mouse = Input.GetAxis("Mouse Y");
+
+            //calculate target rotation
+            Quaternion t_xadj = Quaternion.AngleAxis(-intensity * t_x_mouse, Vector3.up);
+            Quaternion t_yadj = Quaternion.AngleAxis(intensity * t_y_mouse, Vector3.right);
+            Quaternion t_rotation = origin_rotation * t_xadj * t_yadj;
+
+            //rotate towards target rotation
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, t_rotation, Time.deltaTime * smooth);
+        }
+        #endregion
+    }
+
+}
